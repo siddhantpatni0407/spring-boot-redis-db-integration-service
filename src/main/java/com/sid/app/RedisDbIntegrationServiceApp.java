8cac3e03a070b7c12560460231c7bc14a3972564
Siddhant Patni <siddhant4patni@gmail.com>
2025-08-27 14:43:01 +0530

    Spring Boot Redis DB Integration
    Spring Boot Redis DB Integration

---
+++ src/main/java/com/sid/app/RedisDbIntegrationServiceApp.java
@@ -0,0 +1,13 @@
+package com.sid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDbIntegrationServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDbIntegrationServiceApp.class, args);
+    }
+
+}
